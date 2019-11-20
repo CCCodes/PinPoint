6970ae8345e1d1610d66e318da9ce247cd784a84
CCCodes <caitlinchou@gmail.com>
2019-11-19 22:46:46 -0500

    visually -> visual

--- PinPoint/settings.py
+++ PinPoint/settings.py
@@ -177,5 +177,5 @@ EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASS')
-EMAIL_HOST_USER = 'proconduck@gmail.com'
+EMAIL_HOST_USER = 'pinpointlabsllc@gmail.com'
 EMAIL_PORT = 587
